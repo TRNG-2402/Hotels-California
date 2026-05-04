836d0d3a5e31195385e5620efd4efe87718af73b
LuisHimes <90065535+LuisHimes@users.noreply.github.com>
2026-05-04 15:33:01 -0400

    Changed appsettings.Development.json

--- HotelsCaliforia.API/Data/AppDbContext.cs
+++ HotelsCaliforia.API/Data/AppDbContext.cs
@@ -14,16 +14,22 @@ public class AppDbContext : DbContext
     internal DbSet<Invoice> Invoices { get; set; }
     internal DbSet<InvoiceLineItem> InvoiceLineItems { get; set; }
     internal DbSet<Reservation> Reservations { get; set; }
-    
-    public AppDbContext() : base() {}
 
-    public AppDbContext(DbContextOptions options) : base(options) {}
+    public AppDbContext() : base() { }
+
+    public AppDbContext(DbContextOptions options) : base(options) { }
+
+    // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    // {
+    //     optionsBuilder
+    //         .UseSQL
+    // }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         // Component key for room
-        modelBuilder.Entity<Room>( entity =>
-            entity.HasKey(r => new {r.RoomNumber, r.HotelId})
+        modelBuilder.Entity<Room>(entity =>
+            entity.HasKey(r => new { r.RoomNumber, r.HotelId })
         );
         modelBuilder.Entity<User>()
             .HasDiscriminator<string>("User_type")
