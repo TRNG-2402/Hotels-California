872ce75a0e1b52f280ca27e7010380880e785d84
ViridianRock <thomasrocktuck@gmail.com>
2026-05-04 14:54:31 -0400

    Added reservation model

---
+++ HotelsCaliforia.API/Models/Reservtion.cs
@@ -0,0 +1,42 @@
+namespace HotelsCalifornia.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Reservation
+{
+    [Key]
+    public int ReservationId { get; set; }
+
+    [ForeignKey("Member")]
+    public int MemberId { get; set; }
+
+    [ForeignKey("Room")]
+    [Required]
+    public int RoomNumber { get; set; }
+
+    [ForeignKey("Room")]
+    [Required]
+    public int HotelId { get; set; }
+
+    [Required]
+    public required DateTime CheckInTime { get; set; }
+
+    public DateTime CheckOutTime { get; set; }
+
+    [Required]
+    public required string DriversLicense { get; set; }
+
+    [Required]
+    public required string Email { get; set; }
+
+    [Required]
+    public int PhoneNumber { get; set; }
+
+    public bool IsCanceled { get; set; } = false;
+
+
+    public Member? Member { get; set; }
+    [Required]
+    public Room Room { get; set; } = null!;
+
+}
