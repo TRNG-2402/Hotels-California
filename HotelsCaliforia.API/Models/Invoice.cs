820f3782438caa6cee98cdbd1873d29e485de766
Son1ceQY <hr19990128@gmail.com>
2026-05-04 14:26:49 -0400

    Update Invoice.cs

--- HotelsCaliforia.API/Models/Invoice.cs
+++ HotelsCaliforia.API/Models/Invoice.cs
@@ -6,9 +6,19 @@ Is Paid?
 
 */
 
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace HotelsCalifornia.Models;
 
 public class Invoice
 {
+    [Key]
+    public int Id { get; set; }
+    [ForeignKey("Member")]
+    public int? MemberId { get; set; }
+    [ForeignKey("Reservation")]
+    public List<int> ReservationId { get; set; } = new();
 
+    public bool IsPaid { get; set; } = false;
 }
