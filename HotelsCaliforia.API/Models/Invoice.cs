6cfccd44e5cd3c7e0337fbba212d94767acbb128
Son1ceQY <hr19990128@gmail.com>
2026-05-04 14:20:18 -0400

    Added Invoice (work in progress)

---
+++ HotelsCaliforia.API/Models/Invoice.cs
@@ -0,0 +1,14 @@
+/*
+Invoice ID (PK)
+Member ID (FK, optional)
+Reservation ID (FK, one fee to many reservations)
+Is Paid?
+
+*/
+
+namespace HotelsCalifornia.Models;
+
+public class Invoice
+{
+
+}
