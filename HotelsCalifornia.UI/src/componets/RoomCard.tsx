fb51be676545d4090f097d55034aa7f50f2662a2
LuisHimes <90065535+LuisHimes@users.noreply.github.com>
2026-05-12 15:39:21 -0400

    Added RoomCard

---
+++ HotelsCalifornia.UI/src/componets/RoomCard.tsx
@@ -0,0 +1,21 @@
+import type { Room } from "../types/Room"
+//TODO make own styles for room
+import styles from './HotelCard.module.css'
+import { Link } from "react-router-dom"
+
+
+interface RoomCardProps
+{
+    room: Room
+}
+export default function RoomCard({ room }: RoomCardProps)
+{
+    return (
+        <div className={styles.card}>
+            <h3>{room.roomNumber}</h3>
+            <p>{room.description}</p>
+            <p>{room.numBeds}</p>
+            <p>{room.dailyRate}</p>
+        </div>
+    )
+}
