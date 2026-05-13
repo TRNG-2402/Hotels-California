6e157eef8f012aa430884d1688d219b9193105be
Son1ceQY <hr19990128@gmail.com>
2026-05-13 18:17:35 -0400

    Updated room page functions. Still work in progress

--- HotelsCalifornia.UI/src/pages/Rooms.tsx
+++ HotelsCalifornia.UI/src/pages/Rooms.tsx
@@ -2,21 +2,21 @@ import RoomCard from "../components/RoomCard"
 import type { Room } from "../types/Room"
 import { useState, useEffect } from "react"
 import { roomService } from "../services/roomService"
+import { useParams } from "react-router-dom";
 // import { useParams } from "react-router-dom";
 
 
 export default function Rooms()
 {
-  // const { hotelId } = useParams();
-  const testHotelId = 1;
-  // const [roomList, setRoomList] = useState<Room[]>([]);
-  // const [isLoading, setIsLoading] = useState(true);
-  // const [error, setError] = useState<string | null>(null);
+  const { hotelId } = useParams();
+  const [roomList, setRoomList] = useState<Room[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
   // assume this is the rooms of specific hotels from /api/Room/room/hotel/{hotelId}
   const testRooms: Room[] = [
     {
-      roomId: 1,
+      id: 1,
       hotelId: 1,
       roomNumber: 101,
       dailyRate: 5,
@@ -24,7 +24,7 @@ export default function Rooms()
       description: "something"
     },
     {
-      roomId: 2,
+      id: 2,
       hotelId: 1,
       roomNumber: 102,
       dailyRate: 4.5,
@@ -34,18 +34,28 @@ export default function Rooms()
   ]
   // useEffect(() =>
   // {
-  //   roomService.getRoomsByHotelId(hotelId!)
-  //     .then((data) => setRoomList(data))
+  //   roomService.getRoomsByHotelId(Number(hotelId))
+  //     .then((data) =>
+  //     {
+  //       console.log("API data =", data);
+  //       console.log("Type =", Array.isArray(data));
+  //       setRoomList(data)
+  //     })
   //     .catch((err) => setError(err.message ?? 'Failed to load rooms'))
   //     .finally(() => setIsLoading(false))
   // }, [hotelId])
-  // useEffect(() =>
-  // {
-  //   roomService.getAllRooms()
-  //     .then((data) => setRoomList(data))
-  //     .catch((err) => setError(err.message ?? 'Failed to load rooms'))
-  //     .finally(() => setIsLoading(false))
-  // })
+  useEffect(() =>
+  {
+    roomService.getAllRooms()
+      .then((data) =>
+      {
+        console.log("API data =", data);
+        console.log("Type =", Array.isArray(data));
+        setRoomList(data)
+      })
+      .catch((err) => setError(err.message ?? 'Failed to load rooms'))
+      .finally(() => setIsLoading(false))
+  }, [])
 
   // if (isLoading) return <main><p>Loading Rooms...</p></main>
   // if (error) return <main><p style={{ color: 'red' }}>{error}</p></main>
@@ -53,26 +63,17 @@ export default function Rooms()
   return (
     <main>
       <h2>Rooms for Hotels</h2>
-      {
-        // roomList.map((r) => (
-        //   <RoomCard key={r.roomId} room={r} />
-        // ))
-        // roomList.length === 0 ?
-        //   (
-        //     <p>Sorry! There's no room for {hotelId}</p>
-        //   ) :
-        //   (
-        //     roomList.map((r) => <RoomCard key={r.roomId} room={r} />)
-        //   )
-        <div>
-          {
-            testRooms.length === 0 ?
-              <p>Sorry! There's no room</p> :
-              testRooms.map((r) => <RoomCard key={r.roomId} room={r} />)
-          }
-        </div>
-      }
+      <div>
+        {
+          roomList.length === 0 ?
+            (
+              <p>Sorry! There's no room for {hotelId}</p>
+            ) :
 
+            roomList.map((r) => <RoomCard key={r.id} room={r} />)
+
+        }
+      </div>
     </main>
   )
 }
--- HotelsCalifornia.UI/src/services/roomService.ts
+++ HotelsCalifornia.UI/src/services/roomService.ts
@@ -2,12 +2,13 @@ import { api } from "./api";
 import type { Room } from "../types/Room";
 
 export const roomService = {
+    // sends GET to http://localhost:xxxx/api/Room
     async getAllRooms(): Promise<Room[]>
     {
-        const response = await api.get<Room[]>('Room');
+        const response = await api.get<Room[]>('/room');
         return response.data;
     },
-    async getRoomsByHotelId(hotelId: string)
+    async getRoomsByHotelId(hotelId: number): Promise<Room[]>
     {
         const response = await api.get<Room[]>(`Room/room/hotel/${hotelId}`);
         return response.data;
