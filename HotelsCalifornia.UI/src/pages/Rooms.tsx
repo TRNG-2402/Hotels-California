3d84c495cb85b3c5c96a23a05791fe43b48353aa
Son1ceQY <hr19990128@gmail.com>
2026-05-13 19:03:59 -0400

    Created RoomsOfHotel.tsx. Now you can see one hotel's all the rooms via /hotels/:hotelId/rooms

--- HotelsCalifornia.UI/src/pages/Rooms.tsx
+++ HotelsCalifornia.UI/src/pages/Rooms.tsx
@@ -2,48 +2,13 @@ import RoomCard from "../components/RoomCard"
 import type { Room } from "../types/Room"
 import { useState, useEffect } from "react"
 import { roomService } from "../services/roomService"
-import { useParams } from "react-router-dom";
-// import { useParams } from "react-router-dom";
-
 
 export default function Rooms()
 {
-  const { hotelId } = useParams();
   const [roomList, setRoomList] = useState<Room[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
-  // assume this is the rooms of specific hotels from /api/Room/room/hotel/{hotelId}
-  const testRooms: Room[] = [
-    {
-      id: 1,
-      hotelId: 1,
-      roomNumber: 101,
-      dailyRate: 5,
-      numBeds: 3,
-      description: "something"
-    },
-    {
-      id: 2,
-      hotelId: 1,
-      roomNumber: 102,
-      dailyRate: 4.5,
-      numBeds: 4,
-      description: "something"
-    }
-  ]
-  // useEffect(() =>
-  // {
-  //   roomService.getRoomsByHotelId(Number(hotelId))
-  //     .then((data) =>
-  //     {
-  //       console.log("API data =", data);
-  //       console.log("Type =", Array.isArray(data));
-  //       setRoomList(data)
-  //     })
-  //     .catch((err) => setError(err.message ?? 'Failed to load rooms'))
-  //     .finally(() => setIsLoading(false))
-  // }, [hotelId])
   useEffect(() =>
   {
     roomService.getAllRooms()
@@ -57,17 +22,17 @@ export default function Rooms()
       .finally(() => setIsLoading(false))
   }, [])
 
-  // if (isLoading) return <main><p>Loading Rooms...</p></main>
-  // if (error) return <main><p style={{ color: 'red' }}>{error}</p></main>
+  if (isLoading) return <main><p>Loading Rooms...</p></main>
+  if (error) return <main><p style={{ color: 'red' }}>{error}</p></main>
 
   return (
     <main>
-      <h2>Rooms for Hotels</h2>
+      <h2>Rooms for All the Hotels!</h2>
       <div>
         {
           roomList.length === 0 ?
             (
-              <p>Sorry! There's no room for {hotelId}</p>
+              <p>Sorry! There's no rooms</p>
             ) :
 
             roomList.map((r) => <RoomCard key={r.id} room={r} />)
---
+++ HotelsCalifornia.UI/src/pages/RoomsOfHotel.tsx
@@ -0,0 +1,43 @@
+import RoomCard from "../components/RoomCard"
+import type { Room } from "../types/Room"
+import { useState, useEffect } from "react"
+import { roomService } from "../services/roomService"
+import { useParams } from "react-router-dom";
+
+export default function RoomsOfHotel()
+{
+    const { hotelId } = useParams();
+    const [roomList, setRoomList] = useState<Room[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() =>
+    {
+        roomService.getRoomsByHotelId(Number(hotelId))
+            .then((data) =>
+            {
+                console.log("API data =", data);
+                console.log("Type =", Array.isArray(data));
+                setRoomList(data)
+            })
+            .catch((err) => setError(err.message ?? 'Failed to load rooms'))
+            .finally(() => setIsLoading(false))
+    }, [hotelId])
+
+    if (isLoading) return <main><p>Loading Rooms...</p></main>
+    if (error) return <main><p style={{ color: 'red' }}>{error}</p></main>
+
+    return (
+        <main>
+            <h2>Rooms for Hotel {hotelId}</h2>
+            <div>
+                {
+                    roomList.length === 0 ? (
+                        <p>Sorry! There's no room for id={hotelId}</p>
+                    ) :
+                        roomList.map((r) => <RoomCard key={r.id} room={r} />)
+                }
+            </div>
+        </main>
+    )
+}
