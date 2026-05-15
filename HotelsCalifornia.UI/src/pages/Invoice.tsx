b693fb94a829afe97b9ad9324c3835fad8804842
LuisHimes <90065535+LuisHimes@users.noreply.github.com>
2026-05-15 11:40:47 -0400

    FINISHED INVOICE I THINK

--- HotelsCalifornia.UI/src/pages/Invoice.tsx
+++ HotelsCalifornia.UI/src/pages/Invoice.tsx
@@ -4,50 +4,126 @@ import { invoiceService } from "../services/invoiceService";
 import { useAuth } from "../context/AuthContext";
 import EmptyState from "../components/EmptyState";
 import InvoiceCard from "../components/InvoiceCard";
+import { reservationService } from "../services/reservationService";
+import type { Reservation } from "../types/Reservation";
+import type { NewInvoiceLineItem } from "../types/InvoiceLineItem";
+import { invoiceLineItemService } from "../services/invoiceLineItemService";
 
 
 export default function Invoice() 
 {
     const { user } = useAuth();
     const [invoiceList, setInvoiceList] = useState<Invoice[]>([]);
+    const [reservationList, setReservationList] = useState<Reservation[]>([]);
+    const [reservationIdList, setReservationIdList] = useState<number[]>([]);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState<string | null>(null);
     const [unpaid, setUnpaid] = useState(false);
+    const [invoiceId, setInvoiceId] = useState<number>(-1);
+    const [amount, setAmount] = useState<number>(0);
+    const [description, setDescription] = useState("");
+
+    const inputStyle = {
+        width: "100%",
+        padding: "10px",
+        marginTop: "5px",
+        marginBottom: "15px"
+    };
+
+    const buttonStyle = {
+        width: "100%",
+        padding: "12px",
+        backgroundColor: "#aa3bff",
+        color: "white",
+        borderRadius: "6px",
+        cursor: "pointer"
+    };
 
     useEffect(() => 
     {
-        user === null ? (
+        if (user === null)
+        {
             <></>
 
-        ) : (
-            user.role === "Admin" ? (
+        } else
+        {
+            if (user.role === "Manager")
+            {
                 invoiceService.getAllInvoices()
                     .then((data) => setInvoiceList(data))
                     .catch((err) => setError(err.message ?? 'Failed to load Invoice'))
-                    .finally(() => setIsLoading(false))
-            ) : (
+                    .finally(() => setIsLoading(false));
+                reservationService.getReservationsByHotelId(user!.hotelId!)
+                    .then((data) => setReservationList(data))
+                    .catch((err) => setError(err.message ?? 'Failed to load Invoice'))
+                    .finally(() => setIsLoading(false));
+                setReservationIdList(reservationList.map(p => p.reservationId))
+                setInvoiceList(invoiceList.filter((p) => reservationIdList.includes(p.reservationId)))
+            } else
+            {
                 invoiceService.getInvoicesByMemberId(user.userId)
                     .then((data) => setInvoiceList(data))
                     .catch((err) => setError(err.message ?? 'Failed to load Invoice'))
                     .finally(() => setIsLoading(false))
-            )
-        )
+            }
+        }
 
     }, [])
 
-    if (isLoading) return <main><p>Loading products. . . </p></main>
+    if (isLoading) return <main><p>Loading invoices. . . </p></main>
     if (error) return <main><p style={{ color: 'red' }}>{error}</p></main>
-    const filtered = invoiceList.filter((p) => p.isPaid === unpaid)
+    const filtered = invoiceList.filter((p) => p.isPaid === false)
 
     function toggleUnpaid()
     {
         setUnpaid(!unpaid)
+    };
+
+    const clearForm = () =>
+    {
+        setInvoiceId(0);
+        setAmount(0);
+        setDescription("");
+    };
+
+    const handleSubmit = async (
+        e: React.FormEvent<HTMLFormElement>
+    ) =>
+    {
+        e.preventDefault();
+
+        setError(null);
+        setIsLoading(true);
+
+        try
+        {
+            const newInvoiceLineItem: NewInvoiceLineItem = {
+                invoiceId,
+                amount,
+                description
+            };
+
+            await invoiceLineItemService.createInvoiceLineItem(newInvoiceLineItem);
+            clearForm();
+        }
+
+        catch (err: any)
+        {
+            setError(
+                err.response?.data?.message ||
+                "Creation failed"
+            );
+        } finally
+        {
+            setIsLoading(false);
+        };
     }
 
     return (
+
         <main>
             {
-                unpaid ? (
+                !unpaid ? (
                     filtered.length === 0 ? (
                         invoiceList.length === 0 ? (
                             <EmptyState message={'You have no invoices'} />
@@ -61,7 +137,49 @@ export default function Invoice()
                         <>
                             <section>
                                 {filtered.map((p) => (
-                                    <InvoiceCard key={p.invoiceId} invoice={p} />
+                                    <>
+                                        <InvoiceCard key={p.invoiceId} invoice={p} />
+                                        {
+                                            user?.role === "Manager" ? (
+                                                <>
+                                                    <form onSubmit={handleSubmit}>
+                                                        <label>Amount</label>
+                                                        <input
+                                                            type="text"
+                                                            step="0.01"
+                                                            placeholder="0"
+                                                            value={amount}
+                                                            onChange={(e) => { setAmount(Number(e.target.value)); setInvoiceId(p.invoiceId) }}
+                                                            required
+                                                            style={inputStyle}
+                                                        />
+                                                        <label>Description</label>
+                                                        <input
+                                                            type="text"
+                                                            step="0.01"
+                                                            placeholder="Surcharge reason"
+                                                            value={description}
+                                                            onChange={(e) => setDescription(e.target.value)}
+                                                            required
+                                                            style={inputStyle}
+                                                        />
+
+                                                        <button
+                                                            type="submit"
+                                                            disabled={isLoading}
+                                                            style={buttonStyle}
+                                                        >
+                                                            Post Surcharge
+                                                        </button>
+                                                    </form>
+                                                </>
+                                            ) : (
+                                                <>
+
+                                                </>
+                                            )
+                                        }
+                                    </>
                                 ))}
 
                             </section>
@@ -76,12 +194,12 @@ export default function Invoice()
                     ) : (
                         <>
                             <section>
-                                {filtered.map((p) => (
+                                {invoiceList.map((p) => (
                                     <InvoiceCard key={p.invoiceId} invoice={p} />
                                 ))}
 
                             </section>
-                            <button onClick={toggleUnpaid}>View unpaid only</button>
+                            <button onClick={toggleUnpaid}>View unpaid Invoices only</button>
                         </>
                     )
                 )
