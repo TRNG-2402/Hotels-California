b2c6b7a3989530ad91552125758724b5d52cfb4d
Son1ceQY <hr19990128@gmail.com>
2026-05-14 21:11:19 -0400

    Finished room delete function

--- HotelsCalifornia.UI/src/components/RoomCard.tsx
+++ HotelsCalifornia.UI/src/components/RoomCard.tsx
@@ -3,12 +3,18 @@ import type { Room } from "../types/Room"
 import styles from '../styles/HotelCard.module.css'
 import { Link } from "react-router-dom"
 
-
 interface RoomCardProps
 {
     room: Room
 }
-export default function RoomCard({ room }: RoomCardProps)
+
+interface RoomCardWithDeleteProps
+{
+    room: Room
+    onDelete: (id: number) => void;
+}
+
+export function RoomCard({ room }: RoomCardProps)
 {
     return (
         <div className={styles.card}>
@@ -19,6 +25,22 @@ export default function RoomCard({ room }: RoomCardProps)
             <p>Daily Rate: {room.dailyRate}</p>
 
             <Link to={`/rooms/edit/${room.id}`}>Edit</Link>
+        </div>
+    )
+}
+
+export function RoomCardWithDelete({ room, onDelete }: RoomCardWithDeleteProps)
+{
+    return (
+        <div className={styles.card}>
+            <h3>{room.roomNumber}</h3>
+            <p>(Test only)ID: {room.id}</p>
+            <p>{room.description}</p>
+            <p>Number of beds: {room.numBeds}</p>
+            <p>Daily Rate: {room.dailyRate}</p>
+
+            <Link to={`/rooms/edit/${room.id}`}>Edit</Link>
+            <button onClick={() => onDelete(room.id)}>Delete</button>
         </div>
     )
 }
